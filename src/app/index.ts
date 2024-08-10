1a65488b1225951222eec4376adbee9000ae6c88
psyirius <psyirius@gmail.com>
2024-08-10 03:16:37 -0700

    xp

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,13 @@
-air.trace('Hello, world!');
+class App {
+    constructor() {
+        air.trace('Hello, world!');
+    }
+
+    public sayHello() {
+        air.trace('Hello, world!');
+    }
+}
+
+window.onload = () => {
+    new App().sayHello();
+}
